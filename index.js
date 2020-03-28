1a67075c7ee1c425d9880fa9341fc3b605cd4dff
laarchambault <laarchambault@gmail.com>
2020-03-28 11:32:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here
+// Create a variable called multiply that will multiply var1 and var2 whose results will equal "62".
+let var1 = 2;
+let var2 = 31;
+const multiply = var1 * var2;
+
+const random = Math.floor(Math.random() * 10) +1;
+
+const mod = 20 % 8;
+
+const max = Math.max(1, 4, 2, 15, 20);
